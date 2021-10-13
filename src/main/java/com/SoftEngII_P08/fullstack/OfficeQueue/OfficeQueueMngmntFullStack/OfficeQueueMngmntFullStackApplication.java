acc3248575a3498c3f7f7c7d6259ff883e44c6af
Rocco Luca Iamello s282145 <roccoluca@hotmail.it>
2021-10-13 11:24:10 +0200

    Excluded sprint db config

--- src/main/java/com/SoftEngII_P08/fullstack/OfficeQueue/OfficeQueueMngmntFullStack/OfficeQueueMngmntFullStackApplication.java
+++ src/main/java/com/SoftEngII_P08/fullstack/OfficeQueue/OfficeQueueMngmntFullStack/OfficeQueueMngmntFullStackApplication.java
@@ -1,8 +1,13 @@
 package com.SoftEngII_P08.fullstack.OfficeQueue.OfficeQueueMngmntFullStack;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
+//Exclude use of db for sprint
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
 @SpringBootApplication
 public class OfficeQueueMngmntFullStackApplication {
 
