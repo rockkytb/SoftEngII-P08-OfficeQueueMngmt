32782c79c5dd18feb536459ac0cc8aa964efb0b7
Rocco Luca Iamello s282145 <s282145@studenti.polito.it>
2021-10-13 10:33:03 +0200

    Initial commit files

---
+++ src/main/java/com/SoftEngII_P08/fullstack/OfficeQueue/OfficeQueueMngmntFullStack/OfficeQueueMngmntFullStackApplication.java
@@ -0,0 +1,13 @@
+package com.SoftEngII_P08.fullstack.OfficeQueue.OfficeQueueMngmntFullStack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeQueueMngmntFullStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficeQueueMngmntFullStackApplication.class, args);
+	}
+
+}
