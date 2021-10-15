2b059714dba573e39e84ffecedd820507f26ee60
Rocco Luca Iamello s282145 <roccoluca@hotmail.it>
2021-10-15 17:00:31 +0200

    Fix query problem

--- src/main/java/com/SoftEngII_P08/fullstack/OfficeQueue/OfficeQueueMngmntFullStack/Repository/TicketRepository.java
+++ src/main/java/com/SoftEngII_P08/fullstack/OfficeQueue/OfficeQueueMngmntFullStack/Repository/TicketRepository.java
@@ -23,7 +23,7 @@ public interface TicketRepository extends JpaRepository<Ticket, Integer> {
         @Param("id") int serviceId
     );*/
 	
-	@Query(value=" SELECT C.SERVICE_ID  FROM Counter_service C , Ticket T  WHERE T.service_id=C.service_id AND Counter_id=:id AND Served=0   GROUP BY C.service_id")   //  HAVING COUNT(*)= SELECT MAX(CONT)     FROM (SELECT COUNT(*) AS CONT ,C.SERVICE_ID     FROM COUNTER_SERVICE C , TICKET T     WHERE T.SERVICE_ID=C.SERVICE_ID AND COUNTER_ID=:id AND SERVED=0 GROUP BY C.SERVICE_ID)")
+	@Query(value=" SELECT C.SERVICE_ID  FROM counter_service C , Ticket T  WHERE T.service_id=C.service_id AND Counter_id=:id AND Served=0   GROUP BY C.service_id", nativeQuery = true)   //  HAVING COUNT(*)= SELECT MAX(CONT)     FROM (SELECT COUNT(*) AS CONT ,C.SERVICE_ID     FROM COUNTER_SERVICE C , TICKET T     WHERE T.SERVICE_ID=C.SERVICE_ID AND COUNTER_ID=:id AND SERVED=0 GROUP BY C.SERVICE_ID)")
 	    public int countTicketsInServiceQueue(
 	        @Param("id") int counterId
 	    );
